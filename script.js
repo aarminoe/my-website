9cb80480715168745618da85b9fd7ab079f8e3e5
Aaron Noe <aarminoe@gmail.com>
2022-04-10 20:50:14 -0500

    add arrow key response and href to ul

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "";
 document.querySelector("body").appendChild(h2);
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowUp"){
+        alert("What's up?")
+    }
+})
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowDown") {
+        alert("What did you think was going to happen?")
+    }
+})
+
+function respond() {
+    let str = document.getElementById("text1").value;
+    alert (`${str} is an interesting name.`)
+}
+
+document.addEventListener("keydown", function (e) {
+    if(e.key == "ArrowLeft") {
+        alert("Ok, now you're being ridiculous.")
+    }
+})
+
+document.addEventListener("keydown", function(e){
+    if (e.key === "ArrowRight")
+    alert ("CORRECT!!!")
+})
